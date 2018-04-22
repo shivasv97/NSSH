d85f143ff2a37fc70154da2d29af6a5b8a1b1c3b
shivasv97 <shivasv97@gmail.com>
2018-04-22 23:29:14 +0530

    test files for input redirection
    simple program to add two numbers to demonstrate input redirection.

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+        long int a,b,c;
+        scanf("%ld", &a);
+        scanf("%ld", &b);
+        c = a + b;
+        printf("sum: %ld\n", c);
+        return 0;
+}
